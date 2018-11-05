6567e589b656e1e219366c0a7cdd7d5f66dd306e
“Ben <ben.fondon@gmail.com>
2018-11-05 17:05:13 -0600

    Edited code that fixes the countdown timer - which is now working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,14 @@
 //When the page refreshes, a timer counts down starting at 150 seconds. 
-
+//An alert shows up (stopping the page) notifying the user that time is up.
 setTimeout(function(){
     alert("Sorry - Time's Up!!");
 }, 150000);
 
+//Set variables for current time and the deadline - 150 seconds from when the user enters the page.
 var currentTime = Date.parse(new Date());
 var deadline = new Date(currentTime + 150000);
 
+//Create function to determine the amount of time remaining.
 function getTimeRemaining(){
     var t = Date.parse(deadline) - Date.parse(new Date());
     var seconds = Math.floor((t/1000) % 60);
@@ -15,30 +17,19 @@ function getTimeRemaining(){
         'minutes': minutes,
         'seconds': seconds
     };
-}
-
+};
+//Create a function to start the clock, update it every second, and add that information to the div.
 function initializeClock(){
     var timeinterval = setInterval(function(){
         var t = getTimeRemaining(deadline);
-        $("#clockdiv").text('minutes: ' + t.minutes + '<br>' + 'seconds' + t.seconds + '<br>')();
+        $("#clockdiv").text('minutes: ' + t.minutes + 'seconds' + t.seconds)();
         if(t.total<=0){
             clearInterval(timeinterval);
         }
-    },150000);
+    },1000);
+
 }
-
-initializeClock('clockdiv', deadline);
-    //Timer should probably slide with the page so that it is always visible.
-
-   
-
-   
-
-
-
-
-
-
+initializeClock();
 
 
 //If the user submits in time, the page refreshes to show that you are done, along with correct/incorrect answers.
